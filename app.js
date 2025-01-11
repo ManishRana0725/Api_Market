548bf81bd1847ac993c4c134501519b94bd6bc66
ManishRana0725 <manishbadm0725@gmail.com>
2025-01-11 12:07:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const path = require("path");
 const methodOverride = require("method-override");
 //const ejsMate = require("ejs-mate");
 const ejsMate = require("ejs-mate");
+const Listing = require("./models/api.js");
+
+
 
 app.set("views" , path.join(__dirname , "views"));
 app.set("view engine" , "ejs");
@@ -14,13 +17,30 @@ app.use(express.static(path.join(__dirname , "/public")));
 app.engine("ejs", ejsMate);
 
 
+main().then((   )=>{
+    console.log("connected to DB  ");
+    })
+    .catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/api_market');
+};
+
+
+
+
+// home page 
 app.get("/api_market" , (req , res )=>{
     res.render("listings/home.ejs");
 })
-
-app.get("/apiListings" , (req , res)=>{
-    res.render("listings/allListing.ejs");
+ // to show all the APIs
+app.get("/apiListings" , async(req , res)=>{
+    const allListings =  await Listing.find({});
+    res.render("listings/allListing.ejs" , {allListings});
+    //console.log("send listings");
 })
+
+
 
 app.listen(8080 , ()=>{
     console.log("Listening to port 8080 API  ");
