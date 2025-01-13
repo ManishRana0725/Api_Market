65063f5ac90d78627d200975b6cf5c393d1dce29
ManishRana0725 <manishbadm0725@gmail.com>
2025-01-14 02:32:32 +0530

    Create apiListing.js

---
+++ routes/apiListing.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+// const UserCtrl = require("../controller/UserCtrl");
+// abhi kuch nahi hua hai
+// router.post('/register' , UserCtrl.register)
+
+module.exports = router
