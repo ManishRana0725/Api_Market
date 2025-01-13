8863ba9ac2458e613c0c496540055062e2dfe499
ManishRana0725 <manishbadm0725@gmail.com>
2025-01-14 02:31:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,46 +3,37 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
-//const ejsMate = require("ejs-mate");
 const ejsMate = require("ejs-mate");
-const Listing = require("./models/api.js");
+require('dotenv').config();
 
+// Routes
+const apiListingsRoutes = require('./routes/apiListings'); // API listings routes
+const homeRoutes = require('./routes/home'); // Home page routes
+const userRouter = require('./routes/userRoutes');
 
+// Database Connection
+mongoose.connect(process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/api_market', {
+   
+})
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.log("Database Connection Error:", err));
 
-app.set("views" , path.join(__dirname , "views"));
-app.set("view engine" , "ejs");
-app.use(methodOverride("_method"));
-//app.engine("ejs" , ejsMate);
-app.use(express.static(path.join(__dirname , "/public")));
+// Middleware
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(methodOverride("_method"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-
-main().then((   )=>{
-    console.log("connected to DB  ");
-    })
-    .catch(err => console.log(err));
-
-async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/api_market');
-};
-
-
-
-
-// home page 
-app.get("/api_market" , (req , res )=>{
-    res.render("listings/home.ejs");
-})
- // to show all the APIs
-app.get("/apiListings" , async(req , res)=>{
-    const allListings =  await Listing.find({});
-    res.render("listings/allListing.ejs" , {allListings});
-    //console.log("send listings");
-})
-
-
-
-app.listen(8080 , ()=>{
-    console.log("Listening to port 8080 API  ");
-    
+// Routes
+app.use("/", homeRoutes); // Home page routes
+app.use("/apiListings", apiListingsRoutes); // API listings routes
+app.use("/api/users" , userRouter); // API user routes
+// Start the Server
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
+
