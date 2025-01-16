9c554d14f35db0b0f274f96b32b18c1df54ff429
ManishRana0725 <manishbadm0725@gmail.com>
2025-01-16 11:59:37 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
-
-const auth = (req, res, next) => {
+const Users = require('../models/userModel');
+const auth = async (req, res, next) => {
     try {
         // Get the token from the Authorization header
         const token = req.header('Authorization');
@@ -9,9 +9,11 @@ const auth = (req, res, next) => {
         // Verify the token
         const verified = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
         if (!verified) return res.status(401).json({ msg: 'Token verification failed, access denied' });
-
-        // Attach user ID to the request object
-        req.user = verified.id;
+    
+        const user = await Users.findById(verified.id); // Validate token ID matches a user
+    
+        if (!user) return res.status(404).json({ msg: 'User not found' });
+        req.user = user; // Attach user to the request
         next();
     } catch (err) {
         res.status(500).json({ msg: err.message });
