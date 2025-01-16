c421261c18a1d8ec5e64f1571fcb0bd94660c5d7
ManishRana0725 <manishbadm0725@gmail.com>
2025-01-16 11:59:54 +0530

    Update authAdmin.js

--- middleware/authAdmin.js
+++ middleware/authAdmin.js
@@ -2,7 +2,7 @@ const Users = require('../models/userModel')
 
 const authAdmin = async (req , res ,next)=>{
     try{
-        const user = req.Users.findOne({
+        const user = await  Users.findOne({
             _id: req.user.id
         })
 
