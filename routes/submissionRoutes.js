809bd248ad7abfc186611d8ac01b0d00de7c3593
ManishRana0725 <manishbadm0725@gmail.com>
2025-01-17 10:47:05 +0530

    Update submissionRoutes.js

--- routes/submissionRoutes.js
+++ routes/submissionRoutes.js
@@ -1,1 +1,23 @@
 
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const authAdmin = require('../middleware/authAdmin');
+const SubmissionCtrl = require('../controllers/submissionCtrl');
+
+// Add a new submission (User)
+router.post('/', auth, SubmissionCtrl.addSubmission);
+
+// Get all submissions (Admin)
+router.get('/all', auth, authAdmin, SubmissionCtrl.getAllSubmissions);
+
+// Approve a submission (Admin) id of submission
+router.put('/approve/:id', auth, authAdmin, SubmissionCtrl.approveSubmission);
+
+// Reject a submission (Admin) id of submission
+router.delete('/reject/:id', auth, authAdmin, SubmissionCtrl.rejectSubmission);
+
+// Get a specific submission (Admin) 
+router.get('/:id', auth, authAdmin, SubmissionCtrl.getSubmissionById);
+
+module.exports = router;
