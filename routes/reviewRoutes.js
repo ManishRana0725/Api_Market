c97de26b58a0485221ae301b706e387be334cf7e
ManishRana0725 <manishbadm0725@gmail.com>
2025-01-20 12:11:09 +0530

    Update reviewRoutes.js

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const reviewCtrl = require('../controllers/reviewCtrl');
+const auth = require('../middleware/auth');
 
+const router = express.Router();
+
+router.post('/api/:apiId/reviews', auth, reviewCtrl.addReview); // Add a review
+router.get('/api/:apiId/reviews', reviewCtrl.getReviews); // Get reviews
+router.delete('/api/:apiId/reviews/:reviewId', auth, reviewCtrl.deleteReview); // Delete a review
+router.patch('/api/:apiId/reviews/:reviewId', auth, reviewCtrl.updateReview); // Update a review
+
+module.exports = router;
