fd942501a2de174e23d63aba72e9b023e7712a92
ManishRana0725 <manishbadm0725@gmail.com>
2025-01-20 12:10:31 +0530

    Update reviewModel.js

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,1 +1,31 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+    apiId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Api',
+        required: true,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true,
+    },
+    reviewText: {
+        type: String,
+        default: '',
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('Review', reviewSchema);
 
